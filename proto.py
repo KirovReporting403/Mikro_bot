7c59a12e9a8e1f4b5a5a9b636199d9cdbf95b036
M. Aldin Yulis Setiawan <43176061+JustHumanz@users.noreply.github.com>
2019-11-01 01:32:02 +0700

    sensor the private information

--- proto.py
+++ proto.py
@@ -1,11 +1,11 @@
 import paramiko,time,telebot,os,re
 
-k = paramiko.RSAKey.from_private_key_file("/home/humanz/ssh/backup/mikrotik.pem")
+k = paramiko.RSAKey.from_private_key_file("<<--Your RSA key-->>")
 client = paramiko.SSHClient()
 client.load_system_host_keys()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.load_host_keys('/home/humanz/.ssh/known_hosts')
-client.connect('7cbd0896897a.sn.mynetname.net', username='Humanz',pkey = k, port=8090)
+client.load_host_keys('<<--Your Know Host-->>')
+client.connect('<<--IP address or domain-->>', username='Humanz',pkey = k, port=8090)
 stdin, stdout, stderr = client.exec_command("/system script run dhclient")
 result = stdout.read().decode('utf-8').strip("\n")
 print(result)
